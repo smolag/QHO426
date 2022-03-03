414c8f38dc2d4a90914955f3eb3a39ab4ccbc606
smolag <>
2022-03-03 12:05:39 +0000

    drStrange while

---
+++ demos/w2_while.py
@@ -0,0 +1,7 @@
+x = 1
+
+while x <= 12:
+  print(f"Potato numero {x:>2}")
+  x += 1
+else:
+  print("Out of the loop!")
---
+++ demos/w2_while2.py
@@ -0,0 +1,7 @@
+answer = str()
+
+while answer.lower() != "stop":
+  print("Doramu, I came to bargain!")
+  answer = input()
+else:
+  print("You won, dr. Strange!")
